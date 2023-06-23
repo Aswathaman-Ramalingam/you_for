7419e99dddd0afc62eb1f7f32d57ad21e4b31f8c
SAKTHI7R <pyrospyp@gmail.com>
2023-06-23 16:41:14 +0530

    till

--- apps/web/app/home/nav_bar.tsx
+++ apps/web/app/home/nav_bar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Login from '../login/page';
 export default function Navigation() {
   const [activeLink, setActiveLink] = useState('Home');
 
@@ -12,13 +12,13 @@ export default function Navigation() {
       <div className="container ml-auto px-4">
         <div className="flex items-center justify-between h-16">
           
-          <div className="fixed right-8">
+          <div className="fixed right-7">
             
             <a
               className={`${
                 activeLink === 'Contact' ? 'text-white' : 'text-gray-300'
               } hover:text-white px-3 py-2`}
-              href="#"
+              href={'/'}
               onClick={() => handleClick('log out')}
             >
               Log Out
